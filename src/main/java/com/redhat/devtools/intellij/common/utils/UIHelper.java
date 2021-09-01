ff747ce24b8d5c81ab5c52159787625078c4563e
Jeff MAURY <jmaury@redhat.com>
2021-09-01 09:25:20 +0200

    feat: Add new method to detect if current theme/LAF is dark or not (#96)
    * feat: Add new method to detect if current theme/LAF is dark or not
    
    Fixes #95
    
    Signed-off-by: Jeff MAURY <jmaury@redhat.com>

--- src/main/java/com/redhat/devtools/intellij/common/utils/UIHelper.java
+++ src/main/java/com/redhat/devtools/intellij/common/utils/UIHelper.java
@@ -10,8 +10,11 @@
  ******************************************************************************/
 package com.redhat.devtools.intellij.common.utils;
 
+import com.intellij.ide.ui.LafManager;
+import com.intellij.ide.ui.laf.darcula.DarculaLaf;
 import com.intellij.openapi.application.ApplicationManager;
 
+import javax.swing.UIManager;
 import java.util.function.Supplier;
 
 public class UIHelper {
@@ -31,5 +34,10 @@ public class UIHelper {
       ApplicationManager.getApplication().invokeAndWait(() -> val[0] = supplier.get());
       return (T) val[0];
     }
+  }
+
+  public static boolean isDarkMode() {
+    UIManager.LookAndFeelInfo lafInfo = LafManager.getInstance().getCurrentLookAndFeel();
+    return lafInfo.getClassName().equals(DarculaLaf.class.getName());
   }
 }
