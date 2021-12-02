ffe96afaf1219ca6e459f7e16a4ebe64b77ec83b
Jeff MAURY <jmaury@redhat.com>
2021-12-02 12:43:46 +0100

    feat: add VfsRootAccesshelper for IntelliJ 2021.3 compatibility (#123)
    * feat: add VfsRootAccesshelper for IntelliJ 2021.3 compatibility
    
    Fixes #122
    
    Signed-off-by: Jeff MAURY <jmaury@redhat.com>

---
+++ src/test/java/com/redhat/devtools/intellij/common/utils/VfsRootAccessHelper.java
@@ -0,0 +1,44 @@
+/*******************************************************************************
+ * Copyright (c) 2021 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v20.html
+ *
+ * Contributors:
+ * Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+package com.redhat.devtools.intellij.common.utils;
+
+import com.intellij.openapi.Disposable;
+import com.intellij.openapi.vfs.newvfs.impl.VfsRootAccess;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
+public class VfsRootAccessHelper {
+    private VfsRootAccessHelper() {
+    }
+
+    public static void allowRootAccess(Disposable disposable, String... roots) {
+        if (disposable == null) {
+            disposable = () -> {
+            };
+        }
+        try {
+            Method method = Arrays.stream(VfsRootAccess.class.getDeclaredMethods()).filter(m -> m.getName().equals("allowRootAccess")).findFirst().get();
+            if (method.getParameterCount() > 1) {
+                method.invoke(null, disposable, roots);
+            } else {
+                method.invoke(null, roots);
+            }
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void allowRootAccess(String... roots) {
+        allowRootAccess(null, roots);
+    }
+}
