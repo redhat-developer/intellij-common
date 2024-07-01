ecae1360592895cad4db8a77ec95fba0867e9f2a
Stephane Bouchet <sbouchet@redhat.com>
2024-07-01 17:57:58 +0200

    add system property to override download path
    Signed-off-by: Stephane Bouchet <sbouchet@redhat.com>

--- src/main/java/com/redhat/devtools/intellij/common/utils/DownloadHelper.java
+++ src/main/java/com/redhat/devtools/intellij/common/utils/DownloadHelper.java
@@ -39,7 +39,6 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.StringReader;
 import java.net.URL;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
